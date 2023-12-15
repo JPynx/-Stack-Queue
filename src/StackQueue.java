df90587aa2db648b601634925cf3dcaf88ebd312
JPynx <jsanchez-espino24@kentdenver.org>
2023-12-15 10:00:21 -0700

    More code added to StackQueue.java

--- src/StackQueue.java
+++ src/StackQueue.java
@@ -1,3 +1,30 @@
-public class StackQueue<T> {
+public class StackQueue<E> {
+    //instance data
+    private int size;
+    private Stack stack;
+    private Stack stack2;
 
+    //constucotrs
+
+    public StackQueue(){
+        size = 0;
+        stack = new Stack();
+        stack2 = new Stack();
+    }
+
+    public void enqueue(E stackData){
+        stack.push(stackData);
+    }
+
+    public void dequeue (){
+        while (!stack.isEmpty()){
+            stack2.push(stack);
+        }
+
+        if (stack2.isEmpty()){
+            System.out.println("The queue is empty");
+        }
+
+
+    }
 }
